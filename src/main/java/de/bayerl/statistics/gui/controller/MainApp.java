af23d31c8085a679b31cf66b159d468e6e2ec750
googgle <guggenbe@fim.uni-passau.de>
2015-04-23 23:18:01 +0200

    MainView editiert und MainApp geaddet

---
+++ src/main/java/de/bayerl/statistics/gui/controller/MainApp.java
@@ -0,0 +1,16 @@
+package controller;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
